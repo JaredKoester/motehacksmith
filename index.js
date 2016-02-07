a238b8aa275808f210cdb5f15bf0702930c1fa12
Jared.Koester <jaredjkoester@gmail.com>
2016-02-07 01:08:10 -0500

    added body-parser module and used it to make a form that uses a post-request that is logged to the console

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ var handlebars = require('express-handlebars');
 var app = express();
 var view = handlebars.create({ defaultLayout: 'main' });
 
+// Body Parser:
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 app.engine('handlebars', view.engine);
 app.set('view engine', 'handlebars');
 
-app.get('', function (req, res) {
+app.get('/', function (req, res) {
   res.render('hack');
 });
+
+app.post('/send', function(req, res) {
+	console.log(req.body.submitted_message);
+	res.redirect('/');
+})
 
 app.listen(process.env.PORT || 3000, function(){
 	console.log("Express started");
