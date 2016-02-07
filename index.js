80063b458e80e828d0588eb9fb5fca3d93b2edd8
Jared.Koester <jaredjkoester@gmail.com>
2016-02-07 03:13:10 -0500

    We actually won't be needing session support on the node side. We can use firebase login instead. Revert "reorganized the require statements at the top of index.js. Installed express-session"
    This reverts commit 4f8b787bd3b82f3fd1b69b0524b16537f5dbfc94.

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-//Firebase:
 var Firebase = require("firebase");
 var firebase = new Firebase("https://motehacksmith.firebaseio.com/");
 
-//Express:
 var express = require('express');
-var app = express();
-
-//Handlebars:
 var handlebars = require('express-handlebars');
+
+var app = express();
 var view = handlebars.create({ defaultLayout: 'main' });
 
 // Body Parser:
@@ -17,17 +14,6 @@ app.use(bodyParser.json());
 
 //request:
 var request = require('request');
-
-// Session Support:
-var session    = require('express-session');
-app.use(session({
-  secret: 'octocat',
-  // Both of the options below are deprecated, but should be false
-  // until removed from the library - sometimes, the reality of
-  // libraries can be rather annoying!
-  saveUninitialized: false, // does not save uninitialized session.
-  resave: false             // does not save session if not modified.
-}));
 
 
 app.engine('handlebars', view.engine);
