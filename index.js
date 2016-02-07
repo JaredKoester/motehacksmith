3ab6d878ca6d2d96bb7da9b899a900022767c0a1
Jared.Koester <jaredjkoester@gmail.com>
2016-02-07 00:41:19 -0500

    spun up handlebars and added a default layout and a hack view

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var handlebars = require('express-handlebars');
+
 var app = express();
+var view = handlebars.create({ defaultLayout: 'main' });
+
+app.engine('handlebars', view.engine);
+app.set('view engine', 'handlebars');
 
 app.get('', function (req, res) {
-  res.send('Hello World!');
+  res.render('hack');
 });
 
 app.listen(process.env.PORT || 3000, function(){
